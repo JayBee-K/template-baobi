0ffb1e47401fa5331e2f1494051676e687a78919
Jay <killerdrug007@gmail.com>
2022-12-12 21:26:32 +0700

    Udpate

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,25 @@
 	'use strict';
 	let windowWidth = $(window).width();
 
+	const sliderHero = () => {
+		if ($('#slider-hero').length > 0) {
+			new Swiper('#slider-hero .swiper', {
+				slidesPerView: 1,
+				speed: 350,
+				loop: true,
+				autoplay: {
+					delay: 10000,
+				},
+				navigation: {
+					prevEl: '#slider-hero .hero-button_prev',
+					nextEl: '#slider-hero .hero-button_next'
+				},
+			});
+		}
+	}
+
 	$(function () {
 		$(window).resize(() => windowWidth = $(window).width());
+		sliderHero();
 	});
 })(jQuery);
